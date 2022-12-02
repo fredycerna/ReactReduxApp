8cc799383ae6b6fd6a14ea97bf0b944d6415e099
fredy.cerna <fredy.calderon@pearson.com>
2022-12-02 15:45:27 -0600

    fixing typos

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -16,12 +16,10 @@ module.exports = {
   },
   devServer: {
     stats: "minimal",
-    overlay: "true",
+    overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+    headers: { "Access-Control-Allow-Origin": "*" },
     https: false,
   },
   plugins: [
@@ -35,7 +33,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /(\.css)$/,
